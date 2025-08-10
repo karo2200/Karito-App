8e7953e39f43bbc5fb2e406e1166f922c23b79cf
samira.adibi <samira.adibi@apsaaz.com>
2025-08-10 09:07:52 +0330

    feat: profile UI

---
+++ assets/icons/Percent.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import Svg, { Path, SvgProps } from "react-native-svg";
+
+function PercentIcon(props: SvgProps) {
+  return (
+    <Svg width={24} height={24} viewBox="0 0 24 24" fill="none" {...props}>
+      <Path
+        d="M19 5L5 19M9 7a2 2 0 11-4 0 2 2 0 014 0zm10 10a2 2 0 11-4 0 2 2 0 014 0z"
+        stroke="#000"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </Svg>
+  );
+}
+
+export default PercentIcon;
---
+++ components/organisms/profile/ProfileScreen.tsx
@@ -0,0 +1,153 @@
+import EditIcon from "@/assets/icons/Edit";
+import EmptyProfileIcon from "@/assets/icons/EmptyProfile";
+import LocationIcon from "@/assets/icons/Location";
+import PercentIcon from "@/assets/icons/Percent";
+import ThemedContainer from "@/components/atoms/ThemedContainer";
+import ThemedText from "@/components/atoms/ThemedText";
+import { Colors } from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import * as React from "react";
+import {
+  Dimensions,
+  Pressable,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+const { height } = Dimensions.get("window");
+
+export default function ProfileScreen() {
+  const router = useRouter();
+  return (
+    <ThemedContainer>
+      <View style={styles.flex1}>
+        <View style={styles.headerContainer}>
+          <EmptyProfileIcon />
+          <View style={{ flex: 1, paddingHorizontal: 8 }}>
+            <ThemedText
+              fontType="bold"
+              style={{ color: Colors.semiBlack, fontWeight: "600" }}
+            >
+              امیر معتمدنیا
+            </ThemedText>
+            <ThemedText fontType="bold" style={styles.number}>
+              0912-3276543
+            </ThemedText>
+          </View>
+          <EditIcon />
+        </View>
+        <List
+          title="مدیریت آدرس‌ها"
+          icon={<LocationIcon />}
+          onPress={() => {}}
+        />
+        <List title="تخفیف‌ها" icon={<PercentIcon />} onPress={() => {}} />
+        <Pressable style={styles.rowView}>
+          <Ionicons name="log-out-outline" size={24} color={Colors.danger600} />
+          <ThemedText
+            fontType="bold"
+            style={{ color: Colors.danger600, marginRight: 4 }}
+          >
+            خروج از حساب کاربری
+          </ThemedText>
+        </Pressable>
+      </View>
+      <TouchableOpacity
+        activeOpacity={0.7}
+        style={styles.payment}
+        onPress={() => router.push("/home")}
+      >
+        <ThemedText fontType="bold" style={styles.textBtn}>
+          ورود به عنوان متخصص
+        </ThemedText>
+        <Ionicons name="log-in-outline" size={24} color={Colors.hint500} />
+      </TouchableOpacity>
+    </ThemedContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  flex1: { flex: 1 },
+
+  textBtn: {
+    color: Colors.hint500,
+    marginRight: 6,
+  },
+
+  payment: {
+    borderColor: Colors.hint500,
+    borderWidth: 1,
+    paddingVertical: 8,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    borderRadius: 4,
+    flexDirection: "row",
+    marginBottom: 44,
+  },
+
+  rowView: {
+    flexDirection: "row-reverse",
+    paddingVertical: 18,
+  },
+
+  listContainer: {
+    flexDirection: "row-reverse",
+    paddingVertical: 16,
+    paddingHorizontal: 5,
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomColor: Colors.grayMedium,
+    borderBottomWidth: 1,
+  },
+
+  headerContainer: {
+    flexDirection: "row-reverse",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: Colors.gray50,
+    padding: 12,
+    borderRadius: 8,
+    marginTop: 16,
+    marginBottom: 24,
+  },
+
+  number: {
+    color: Colors.semiBlack,
+    fontWeight: "600",
+    marginTop: 8,
+  },
+});
+
+const List = ({
+  title,
+  icon,
+  onPress,
+}: {
+  title: string;
+  icon: any;
+  onPress: () => void;
+}) => {
+  return (
+    <View>
+      <Pressable style={styles.listContainer} onPress={onPress}>
+        {icon}
+        <ThemedText
+          fontType="bold"
+          style={{
+            color: Colors.label,
+            marginRight: 4,
+            flex: 1,
+            fontWeight: "600",
+          }}
+        >
+          {title}
+        </ThemedText>
+        <Ionicons name="chevron-back" color={Colors.label} size={16} />
+      </Pressable>
+    </View>
+  );
+};
