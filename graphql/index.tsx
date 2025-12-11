6742a7bd0213608b0602571f9c65672fc50b53c1
samira.adibi <samira.adibi@apsaaz.com>
2025-12-11 23:38:27 +0330

    feat: change qa url

--- graphql/index.tsx
+++ graphql/index.tsx
@@ -1,6 +1,6 @@
 import { GraphQLClient } from "graphql-request";
 
-export const API_URL = "https://dev-api8081.karito.net/graphql";
+export const API_URL = "https://qa-api8082.karito.net/graphql";
 
 export const graphQLClient = new GraphQLClient(API_URL);
 
